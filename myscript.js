8cfb466a1d539b972f711a6a4e7338b8b577bd0b
Nygery <33994452+Nygery@users.noreply.github.com>
2018-01-28 16:47:52 +0100

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,85 @@
+var click = document.querySelector(".box");
+var all = document.body.querySelectorAll('.box');
+
+
+
+click.onclick = function(){
+ 
+  click.classList.toggle("box-resize")
+  all.classList.toggle("box-resize")
+   let.classList.toggle("box-resize")
+  
+}
+
+//////////////////TOP  Toggle class to maximize content img gallery info    ///////////////////
+
+/*
+var oneBlog = document.querySelector(".one.blog");
+var span = document.querySelector(".one.blog span");
+oneBlog.onclick = function(){
+ 
+  span.classList.toggle("max")
+  
+}*/
+
+
+
+//////////////////BOTTOM  Toggle class to maximize content img gallery info    ///////////////////
+
+
+
+
+//////////////////TOP  Toggle class to maximize content img gallery info    ///////////////////
+
+/*
+var fourBlog = document.querySelector(".four.blog");
+var span = document.querySelector(".four.blog span");
+fourBlog.onclick = function(){
+ 
+  span.classList.toggle("max")
+  
+}
+*/
+//////////////////BOTTOM  Toggle class to maximize content img gallery info    ///////////////////
+var oneBlog = document.querySelector(".one.blog");
+var twoBlog = document.querySelector(".two.blog");
+var threeBlog = document.querySelector(".three.blog");
+var fourBlog = document.querySelector(".four.blog");
+var fiveBlog = document.querySelector(".five.blog");
+
+/////////// span content element ////////////// down///
+var span = document.querySelector(".one.blog span");
+var spanA = document.querySelector(".two.blog span");
+var spanB = document.querySelector(".three.blog span");
+var spanC = document.querySelector(".four.blog span");
+var spanD = document.querySelector(".five.blog span");
+
+oneBlog.onclick = function(){
+
+  span.classList.toggle("max") 
+}
+
+twoBlog.onclick = function(){
+ spanA.classList.toggle("max")  
+}
+
+threeBlog.onclick = function(){
+ spanB.classList.toggle("max")  
+}
+
+fourBlog.onclick = function(){
+ spanC.classList.toggle("max")  
+}
+
+fiveBlog.onclick = function(){
+ spanD.classList.toggle("max")  
+}
+
+
+
+
+//////////////////TOP  Toggle class to maximize content img gallery info    ///////////////////
+
+
+//////////////////BOTTOM  Toggle class to maximize content img gallery info    ///////////////////
+
